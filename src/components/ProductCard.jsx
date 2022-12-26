3d29d271cf304b0b330e80c1fda8379ffe280497
sumanthpweb11 <sumanthpweb11@gmail.com>
2022-12-26 18:05:20 +0530

    minor changes in productcard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -17,9 +17,20 @@ const ProductCard = (props) => {
     <>
       {/* CARD 1 */}
       <div
-        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+        className={`${
+          location.pathname === "/product" ? `gr-${grid}` : "col-3"
+        }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname === "/"
+              ? "/product/:id"
+              : location.pathname === "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src={wish} alt="wishlist" />
@@ -68,9 +79,20 @@ const ProductCard = (props) => {
       </div>
       {/* CARD 2 */}
       <div
-        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+        className={`${
+          location.pathname === "/product" ? `gr-${grid}` : "col-3"
+        }`}
       >
-        <Link className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname === "/"
+              ? "/product/:id"
+              : location.pathname === "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src={wish} alt="wishlist" />
